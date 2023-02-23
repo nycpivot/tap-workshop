4f10b03f2c5152e0e479396658b9328f1544f852
Michael James <mijames@vmware.com>
2023-02-22 20:54:37 -0500

    Initial commit.

--- full-profile/01-aws-prereqs.sh
+++ full-profile/01-aws-prereqs.sh
@@ -2,4 +2,4 @@
 
 read -p "AWS Region Code: " aws_region_code
 
-aws cloudformation stack-create-complete --region $aws_region_code --stack-name tap-workshop-singlecluster-stack --template-body file:///home/ubuntu/tanzu-operations/tap-operations/aws-eks/config/tap-singlecluster-stack.yaml
+aws cloudformation stack-create-complete --region $aws_region_code --stack-name tap-workshop-singlecluster-stack --template-body file:///home/ubuntu/tap-workshop/full-profile/config/tap-singlecluster-stack.yaml
--- multi-profile/01-aws-prereqs.sh
+++ multi-profile/01-aws-prereqs.sh
@@ -2,4 +2,4 @@
 
 read -p "AWS Region Code: " aws_region_code
 
-aws cloudformation stack-create-complete --region $aws_region_code --stack-name tap-workshop-multicluster-stack --template-body file:///home/ubuntu/tanzu-operations/tap-operations/aws-eks/config/tap-multicluster-stack.yaml
+aws cloudformation stack-create-complete --region $aws_region_code --stack-name tap-workshop-multicluster-stack --template-body file:///home/ubuntu/tap-workshop/full-profile/config/tap-multicluster-stack.yaml

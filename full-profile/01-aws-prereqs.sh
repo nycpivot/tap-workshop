35bda8e2c53e17c11eb9b0867ecbc1c67d85e811
Michael James <mijames@vmware.com>
2023-02-24 18:11:34 -0500

    Changed cloudformation to wait till complete.

--- full-profile/01-aws-prereqs.sh
+++ full-profile/01-aws-prereqs.sh
@@ -2,4 +2,4 @@
 
 read -p "AWS Region Code: " aws_region_code
 
-aws cloudformation create-stack --region $aws_region_code --stack-name tap-workshop-singlecluster-stack --template-body file:///home/ubuntu/tap-workshop/full-profile/config/tap-singlecluster-stack.yaml
+aws cloudformation wait stack-create-complete --region $aws_region_code --stack-name tap-workshop-singlecluster-stack --template-body file:///home/ubuntu/tap-workshop/full-profile/config/tap-singlecluster-stack.yaml
